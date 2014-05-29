1c686efdd541a1f92fab48d82103cddbf4a2226c
lyschoening <lars@lyschoening.de>
2014-05-29 11:08:20 +0200

    Implement Diff.__eq__

--- co/difference.py
+++ co/difference.py
@@ -1,7 +1,7 @@
 class Diff(object):
-    def __init__(self, added, removed, changed=None):
-        self.added = tuple(added)
-        self.removed = tuple(removed)
+    def __init__(self, added=None, removed=None, changed=None):
+        self.added = tuple(added) if added else ()
+        self.removed = tuple(removed) if removed else ()
         self.changed = tuple(changed) if changed else None
 
     def __invert__(self):
@@ -9,6 +9,13 @@ class Diff(object):
 
     def __len__(self):
         return len(self.added) + len(self.removed) + (not self.changed or len(self.changed))
+
+    def __eq__(self, other):
+        if not isinstance(other, Diff):
+            return False
+        return self.added == other.added and \
+               self.removed == other.removed and \
+               self.changed == other.changed
 
     def __repr__(self):
         if self.changed:
